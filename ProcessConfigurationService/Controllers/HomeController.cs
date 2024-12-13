18e47b10aa7f15d06361e7aeab7c5ee097c510c6
beatriz.del_saz <beatriz.del_saz@sita.aero>
2024-12-13 16:15:13 +0100

    [#1234] - Initial commit

---
+++ ProcessConfigurationService/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessConfigurationService.Controllers
+{   
+    [ApiController]
+    [Route("/api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(new {
+                message1 = "This is the process configuration service",
+                message2 = "Pinneapple belongs on pizza"
+            });
+        }
+    }
+}
---
+++ TranslationsService/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TranslationsService.Controllers
+{   
+    [ApiController]
+    [Route("/api/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(new {
+                message = "This is the translations service"
+            });
+        }
+    }
+}
